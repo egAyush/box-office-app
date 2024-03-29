83e5d158cf0c1b80a6b0365d8ab293a86282f274
egAyush <ayushpandey3269@gmail.com>
2024-03-29 19:24:50 +0530

    added back to home button on the read more show page and open the read more page in a new window

--- src/pages/Show.jsx
+++ src/pages/Show.jsx
@@ -1,5 +1,5 @@
 import { useQuery } from '@tanstack/react-query';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import { getShowById } from '../api/tvmaze';
 import ShowMainData from '../components/shows/ShowMainData';
 import Details from '../components/shows/Details';
@@ -21,6 +21,8 @@ const Show = () => {
   if (showData) {
     return (
       <div>
+        <Link to="/">Go Back to Home</Link>
+
         <ShowMainData
           image={showData.image}
           name={showData.name}
