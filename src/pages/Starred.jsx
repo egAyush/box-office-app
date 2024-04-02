d3f5aa9360ca2ba737ab4ee8ba346c26bdd0b76b
egAyush <ayushpandey3269@gmail.com>
2024-04-02 11:12:31 +0530

    instructed eslint that we are using es6 so that is doesn't show an error for Promise keyword , fetched starred data and display showgrid for them

--- src/pages/Starred.jsx
+++ src/pages/Starred.jsx
@@ -1,9 +1,36 @@
+import { useQuery } from '@tanstack/react-query';
 import { useStarredShows } from '../lib/useStarredShows';
+import { getShowsByIds } from '../api/tvmaze';
+import ShowGrid from '../components/shows/ShowGrid';
 
 const Starred = () => {
-  const [starredShows] = useStarredShows();
-  //  starredShows
-  return <div>Starred Page, Starred {starredShows.length}</div>;
+  const [starredShowsIds] = useStarredShows();
+
+  const { data: starredShows, error: starredShowsError } = useQuery({
+    queryKey: ['starred', starredShowsIds],
+    queryFn: async () =>
+      getShowsByIds(starredShowsIds).then(result =>
+        result.map(show => ({ show }))
+      ),
+
+    // ⬇️ disabled as long as the filter is empty
+
+    refetchOnWindowFocus: false,
+  });
+  console.log({ starredShows });
+
+  if (starredShows?.length > 0) {
+    return <ShowGrid shows={starredShows} />;
+  }
+  if (starredShows?.length === 0) {
+    return <div>No Shows were Starred</div>;
+  }
+
+  if (starredShowsError) {
+    return <div>Error Occured : {starredShowsError.message}</div>;
+  }
+
+  return <div>Shows are loading</div>;
 };
 
 export default Starred;
